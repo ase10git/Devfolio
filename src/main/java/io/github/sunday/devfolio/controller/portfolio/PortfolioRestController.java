d78248826b88fb37965514e855083902c1a90a24
ase10git <ase1031@naver.com>
2025-08-13 15:48:37 +0900

    refactor(portfolio) : PortfolioRestController RequestMapping 추가(#19)

--- src/main/java/io/github/sunday/devfolio/controller/portfolio/PortfolioRestController.java
+++ src/main/java/io/github/sunday/devfolio/controller/portfolio/PortfolioRestController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +16,7 @@ import java.util.List;
  * 포트폴리오 요청을 처리하는 RestController
  */
 @RestController
+@RequestMapping("/api/portfolio")
 @RequiredArgsConstructor
 public class PortfolioRestController {
     private final PortfolioService portfolioService;
@@ -24,7 +25,7 @@ public class PortfolioRestController {
      * 포트폴리오 목록 출력 API
      * 무한 스크롤에 적용되는 정보 요청 API
      */
-    @GetMapping("/api/portfolio/list")
+    @GetMapping("/list")
     public ResponseEntity<List<PortfolioListDto>> list(
             @ModelAttribute PortfolioSearchRequestDto requestDto
     ) {
