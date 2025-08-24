591610d1572696f2f72b23000cbc40c20f56466c
bangjjun <p01085488742@gmail.com>
2025-08-24 16:10:30 +0900

    feat: 1분마다 만료기간이 지난 이메일 인증 정보 삭제 (#76)

--- src/main/java/io/github/sunday/devfolio/repository/EmailVerificationRepository.java
+++ src/main/java/io/github/sunday/devfolio/repository/EmailVerificationRepository.java
@@ -3,8 +3,11 @@ package io.github.sunday.devfolio.repository;
 import io.github.sunday.devfolio.entity.table.user.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
     Optional<EmailVerification> findTopByEmailOrderByExpiredAtDesc(String email);
+
+    void deleteByExpiredAtBefore(ZonedDateTime now);
 }
